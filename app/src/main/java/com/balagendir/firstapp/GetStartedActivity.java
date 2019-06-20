9a6ee14ab19bb65d056f0b8597170bebfb00fee5
akbar <jamalakbaralam@gmail.com>
2019-06-20 22:25:54 +0700

    (5) Timer/auto event hancdling + toast/alert

--- app/src/main/java/com/balagendir/firstapp/GetStartedActivity.java
+++ app/src/main/java/com/balagendir/firstapp/GetStartedActivity.java
@@ -3,12 +3,28 @@ package com.balagendir.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class GetStartedActivity extends AppCompatActivity {
+
+    EditText input_email;
+    String email_address = "akunpalsu29@gmail.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+
+        input_email = findViewById(R.id.email_address);
+        if (email_address == "akunpalsu29@gmail.com"){
+            input_email.setText(email_address);
+//            ALERT
+            Toast.makeText(getApplicationContext(), "Your Email Address Is Valid", Toast.LENGTH_SHORT).show();
+        }
+        else {
+//            ALERT
+            Toast.makeText(getApplicationContext(), "Your Email Address Is Invalid or Not Registered", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/balagendir/firstapp/MainActivity.java
+++ app/src/main/java/com/balagendir/firstapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,15 +21,18 @@ public class MainActivity extends AppCompatActivity {
 
         applogo = findViewById(R.id.applogo);
 
-        applogo.setOnClickListener(new View.OnClickListener() {
+//        HANDLING EVENT
+        Handler handler = new Handler();
+//        MELAKUKAN EVENT SETELAH 2000ms/2s
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-//                menuju page lain
+            public void run() {
+//                MENUJU APP LAIN
                 Intent next_page = new Intent(MainActivity.this, GetStartedActivity.class);
                 startActivity(next_page);
                 finish();
             }
-        });
+        }, 2000);
 
     }
 }
