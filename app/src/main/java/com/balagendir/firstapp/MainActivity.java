a541c33a5264824b1d9535b47e0a6c9dc80aaa94
akbar <jamalakbaralam@gmail.com>
2019-06-21 17:27:07 +0700

    6c - Design XML Splash Screen

--- app/src/main/java/com/balagendir/firstapp/MainActivity.java
+++ app/src/main/java/com/balagendir/firstapp/MainActivity.java
@@ -12,27 +12,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView applogo;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        applogo = findViewById(R.id.applogo);
-
-//        HANDLING EVENT
-        Handler handler = new Handler();
-//        MELAKUKAN EVENT SETELAH 2000ms/2s
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-//                MENUJU APP LAIN
-                Intent next_page = new Intent(MainActivity.this, GetStartedActivity.class);
-                startActivity(next_page);
-                finish();
-            }
-        }, 2000);
-
-    }
 }
