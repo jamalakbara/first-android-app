2a41b9ae9473b59a8c9220c64a8fe9311d54391b
akbar <jamalakbaralam@gmail.com>
2019-06-22 21:27:26 +0700

    6e - Design XML Sign In Screen

--- app/src/main/java/com/balagendir/firstapp/GetStartedActivity.java
+++ app/src/main/java/com/balagendir/firstapp/GetStartedActivity.java
@@ -2,15 +2,32 @@ package com.balagendir.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class GetStartedActivity extends AppCompatActivity {
+
+    Button btn_sign_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+
+        btn_sign_in = findViewById(R.id.btn_sign_in);
+
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent go_to_sign_in = new Intent(GetStartedActivity.this, SignInActivity.class);
+                startActivity(go_to_sign_in);
+            }
+        });
+
     }
 }
