de6eadff9a98703272f7b928982c44944f1364f1
akbar <jamalakbaralam@gmail.com>
2019-06-23 08:40:09 +0700

    7a - Design XML Register I Screen + benerin splash, sama padding di input

--- app/src/main/java/com/balagendir/firstapp/GetStartedActivity.java
+++ app/src/main/java/com/balagendir/firstapp/GetStartedActivity.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 
 public class GetStartedActivity extends AppCompatActivity {
 
-    Button btn_sign_in;
+    Button btn_sign_in, btn_new_acc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +20,21 @@ public class GetStartedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_get_started);
 
         btn_sign_in = findViewById(R.id.btn_sign_in);
+        btn_new_acc = findViewById(R.id.btn_new_acc);
 
         btn_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent go_to_sign_in = new Intent(GetStartedActivity.this, SignInActivity.class);
                 startActivity(go_to_sign_in);
+            }
+        });
+
+        btn_new_acc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent go_to_regis1 = new Intent(GetStartedActivity.this, RegisterOneActivity.class);
+                startActivity(go_to_regis1);
             }
         });
 
--- app/src/main/java/com/balagendir/firstapp/SignInActivity.java
+++ app/src/main/java/com/balagendir/firstapp/SignInActivity.java
@@ -2,13 +2,29 @@ package com.balagendir.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
+
+    TextView btn_new_acc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        btn_new_acc = findViewById(R.id.new_acc);
+
+        btn_new_acc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent go_to_regis1 = new Intent(SignInActivity.this, RegisterOneActivity.class);
+                startActivity(go_to_regis1);
+            }
+        });
+
     }
 }
