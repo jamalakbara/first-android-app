eed432531c5a700fa7709198974b093ac648b965
akbar <jamalakbaralam@gmail.com>
2019-06-23 10:33:18 +0700

    benerin btn continue sama back. nambahain onbackpressed buat btn back

--- app/src/main/java/com/balagendir/firstapp/RegisterOneActivity.java
+++ app/src/main/java/com/balagendir/firstapp/RegisterOneActivity.java
@@ -3,12 +3,26 @@ package com.balagendir.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.content.Intent;
 
 public class RegisterOneActivity extends AppCompatActivity {
+
+    LinearLayout btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_one);
+
+        btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
     }
 }
